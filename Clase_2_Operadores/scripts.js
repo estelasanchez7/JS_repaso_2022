01e79bbae565bf3954bef8f63aabc0f17565fc07
estelasanchez7 <stelasv7@gmail.com>
2022-09-02 11:52:26 +0200

    committed

---
+++ Clase_2_Operadores/scripts.js
@@ -0,0 +1,37 @@
+// Operadores matemáticos
+
+/* let cadena = "hola mundo";
+
+console.log(cadena) 
+
+let a = 5;
+let b = 2;
+
+let usuario = 'Estela'
+
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+console.log('Hola ' + usuario)*/ 
+
+//Operadores Incremento/Decremento
+
+/*let a = 5;
+
+console.log(a++)
+console.log(a)
+console.log(a)
+console.log(a++)
+console.log(a)
+console.log(++a)
+console.log(++a)
+console.log(++a)
+console.log(a++)
+console.log(a++)
+console.log(a)
+console.log(a)
+console.log(++a)*/
---
+++ Clase_3_Strings/scripts.js
@@ -0,0 +1,9 @@
+/*Strings*/
+
+let cadena = 'Hola mundo';
+
+/*PROPIEDADES
+length -> Devuelve la longitud de la cadena
+*/
+
+console.log(cadena.length);
