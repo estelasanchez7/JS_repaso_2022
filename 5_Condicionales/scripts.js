f1b8472796054be8f27d4670037e4ca802ad6b87
estelasanchez7 <stelasv7@gmail.com>
2022-09-03 17:03:32 +0200

    Condicionales

---
+++ 5_Condicionales/scripts.js
@@ -0,0 +1,14 @@
+/*Condicional simple */
+let num = -5
+
+if(num>0){
+    console.log(`${num} es mayor que 0`)
+}
+
+/*Condicional compuesto */
+
+if(num>0){
+    console.log(`${num} es mayor que 0`)
+}else{
+    console.log(`${num} es menor que 0`)
+}
