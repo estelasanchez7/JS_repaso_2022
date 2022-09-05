66ad0ba469db82eff7615e73a71c90292322d04c
estelasanchez7 <stelasv7@gmail.com>
2022-09-05 13:28:55 +0200

    12_BucleFor

---
+++ 12_BucleFor/scripts.js
@@ -0,0 +1,19 @@
+/* Bucles */
+
+/* 
+    Determinados
+
+    bucle for
+
+    Su sintaxis se compone de 3 partes
+        Iniciación de variable
+        Número de vueltas
+        Incremento o decremento
+
+    for(let i=0;i<=10;i++){
+        Código a ejecutar
+    }
+*/
+
+
+
