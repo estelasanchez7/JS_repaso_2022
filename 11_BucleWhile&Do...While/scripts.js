d3fec5057364ac01f3c4268406b9a5ab1e95a537
estelasanchez7 <stelasv7@gmail.com>
2022-09-05 12:25:49 +0200

    11_BucleWhile

---
+++ 11_BucleWhile&Do...While/scripts.js
@@ -0,0 +1,29 @@
+/* Bucles */
+
+/* 
+    Indeterminados - No sabemos el número de veces que se va a ejecutar el código
+    Bucle while
+    while(condición){
+        //código a ejecutar
+    }    
+    Bucle do...while
+    do{
+        //código a ejecutar
+    }while(condición)
+*/
+
+let pass = '';
+/*
+while(pass != 'hola'){
+    pass = prompt('Introduzca su contraseña')
+};
+
+
+console.log('fin del bucle')*/
+
+/*do{
+    pass = prompt('introduzca password')
+
+}while(pass != 'hola')
+
+console.log('fin bucle')*/
