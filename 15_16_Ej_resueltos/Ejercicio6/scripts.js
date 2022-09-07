e3346dd8827fc310671b8e1bae2d23a00823aeb8
estelasanchez7 <stelasv7@gmail.com>
2022-09-07 16:27:08 +0200

    Ejercicio6

---
+++ 15_16_Ej_resueltos/Ejercicio6/scripts.js
@@ -0,0 +1,14 @@
+/* EJERCICIO 6
+- Escribe un programa que permita ir introduciendo una serie indeterminadade números mientras su suma no supere 50. Cuando esto ocurra, se debe mostrar el total acumulado y el contador de cuantos números se han introducido
+*/
+
+let suma=0
+let cont=0
+
+while(suma<50){
+    suma+=parseInt(prompt('Introduce un nº para añadir a la suma'))
+    cont++
+}
+
+console.log(`La suma total es ${suma}`)
+console.log(`El total de números introducidos es: ${cont}`)
