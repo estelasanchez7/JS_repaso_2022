74cd0dcfc86b65c80baee63e38e38e295f54741f
estelasanchez7 <stelasv7@gmail.com>
2022-09-07 14:02:15 +0200

    Ejercicio5

---
+++ 15_16_Ej_resueltos/Ejercicio5/scripts.js
@@ -0,0 +1,16 @@
+/* EJERCICIO 5
+- Escriba un programa que pida un número entero mayor que cero y calcule su factorial.
+El factorial es el resultado de multiplicar ese número por sus anteriores hasta la unidad.
+
+!5 = 5*4*3*2*1 = 120
+
+*/
+
+let num = parseInt(prompt('Introduce un nº mayor que 0'))
+let result = 1
+
+for(let i=num; i>0; i--){
+    result *= i
+}
+
+console.log(`El factorial de ${num} es ${result}`)
