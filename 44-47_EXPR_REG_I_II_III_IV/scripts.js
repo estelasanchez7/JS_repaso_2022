8e080e4bde6402440fb46eaff64403661a589e3b
estelasanchez7 <stelasv7@gmail.com>
2022-10-05 10:53:31 +0200

    46_47_EXPR_REG_II_III

--- 44-47_EXPR_REG_I_II_III_IV/scripts.js
+++ 44-47_EXPR_REG_I_II_III_IV/scripts.js
@@ -12,12 +12,16 @@
 
     Comodines:
         -Sustitución: Define un comodín dentro del patron. El símbolo es el "."
-        -Listado de caracteres válidos: Entre corchetes se pone una lista de los caracteres válidos. 
+
+        -Listado de caracteres válidos: Entre corchetes se pone una lista de los caracteres válidos.
         [aeiou] Con esto cogeríamos todas las vocales
+
         -Rangos: Entre corchetes si ponemos un guión entre dos caracteres establecemos un rango. [a-z] Todas las letras minúsculas.
         Tabla ASCII https://ascii.cl/es/
+
         -Mezcla entre rangos y listas: 
             Podemos unir los dos anteriores en una sola expresión. [0-5ou] Serían números del 0 al 5, la letra "o" y la letra "u"
+
         -Cadenas completas:
             Para establecer una cadena completa debe ir entre paréntesis, si queremos más palabras irán separadas por un pipe. (lorem|amet) es válida la palabra "lorem" y la palabra "amet"
 
@@ -27,15 +31,15 @@
         ^hola$
         
     Cantidad:
-        - llaves: lo que está antes tiene que aparecer la cantidad exacta de veces. Hay tres combinaciones posibles. 
+        -{} Llaves: lo que está antes tiene que aparecer la cantidad exacta de veces. Hay tres combinaciones posibles. 
             {n} Se tiene que repetir n veces
             {n,m} Se tiene que repetir entre n y m veces, ambas incluidas.
             {n,} Se tiene que repetir como mínimo n veces y sin máximo
         ^[a-zA-Z]{1,3}@{1}$
-        -asterisco: Lo que está antes del asterisco puede estar, puede no estar y se puede repetir.  .*@.*\..*
-        -interrogación: Lo que está antes de la interrogación puede no estar, pero si está solo puede aparecer una vez.
+        -* Asterisco: Lo que está antes del asterisco puede estar, puede no estar y se puede repetir.  .*@.*\..*
+        -? Interrogación: Lo que está antes de la interrogación puede no estar, pero si está solo puede aparecer una vez.
             ^[ae]?$ 
-        - operador +: lo que está antes del + tiene que estár una vez como mínimo
+        - Operador +: lo que está antes del + tiene que estár una vez como mínimo
         A-[0-9]+
     
     Caracteres:
