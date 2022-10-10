7eae8240e517ba9cf4534caa7ea29f5dedf61d68
estelasanchez7 <stelasv7@gmail.com>
2022-10-10 11:39:52 +0200

    54_API_FILE_III_Drag_

---
+++ 54_API_FILE_III_Drag_&_Drop/scripts.js
@@ -0,0 +1,19 @@
+const fileInput = document.querySelector('#file')
+const dropZone = document.querySelector('#drop-zone')
+
+dropZone.addEventListener('dragover', (e) => {
+    e.preventDefault()
+    dropZone.classList.add('drop-zone--active')
+})
+
+dropZone.addEventListener('dragleave', (e) => {
+    e.preventDefault()
+    dropZone.classList.remove('drop-zone--active')
+})
+
+dropZone.addEventListener('drop', (e) => {
+    e.preventDefault()
+    fileInput.files = e.dataTransfer.files
+    console.log(fileInput.files)
+})
+
