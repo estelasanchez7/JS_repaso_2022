7ee2170310c42186cb18158f99efe144dbf6e581
estelasanchez7 <stelasv7@gmail.com>
2022-10-11 12:12:36 +0200

    56_IndexDB_II

---
+++ 56_IndexDB_II/scripts.js
@@ -0,0 +1,44 @@
+/* INDEXDB II: AÑADIENDO REGISTROS
+//Especificación oficial: https://developer.mozilla.org/es/docs/IndexedDB-840092-dup */
+
+const indexedDB = window.indexedDB
+const form = document.getElementById('form')
+
+
+if(indexedDB && form){
+    let db
+    const request = indexedDB.open('tasksList',1)
+
+    request.onsuccess=()=>{
+        db = request.result
+        console.log('open', db)
+    }
+    request.onupgradeneeded=()=>{
+        db = request.result
+        console.log('Create', db)
+        const objectStore = db.createObjectStore('tasks',{
+            keyPath:'taskTitle'
+        })
+   
+    }
+
+    request.onerror=(error)=>{
+        console.log('error',error)
+    }
+
+    const addData = (data) => {
+        const transaction = db.transaction(['tasks'],'readwrite')
+        const objectStore = transaction.objectStore('tasks')
+        const request = objectStore.add(data)
+    }
+
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        const data ={
+            taskTitle:e.target.task.value,
+            taskPriority:e.target.priority.value
+        }
+       
+        addData(data)
+    })
+}
